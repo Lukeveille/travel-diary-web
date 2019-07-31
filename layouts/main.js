fb1728708e490ff7d03a5accd5f8ec3c1ebb6f16
Lukeveille <luke@lukeleveille.com>
2019-07-31 17:19:20 -0400

    setting up server side rendering with next.js

---
+++ layouts/main.js
@@ -0,0 +1,10 @@
+const Main = Page => {
+  return () => (
+    <div>
+      <h1>My Diary</h1>
+      <Page />
+    </div>
+  );
+};
+
+export default Main;
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+import MainLayout from '../layouts/main';
+
+const Page = () => <div>Live and ready</div>;
+
+export default MainLayout(Page);
